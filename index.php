490c522e3a2cc03335d7db56aa65b616d5b7ea85
basantaGhimire <160000892+basantaGhimire@users.noreply.github.com>
2024-02-17 20:00:56 +0545

    Create index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta name="width=device-width, initial-scale=1.0">
+        <meta charset="utf-8">
+        <meta name="keywords" content="buy product in discount">
+        <title>productfree purchase</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <h1>hello world</h1>
+    </body>
+</html> ?>
